0b40f0538768ac34e80e6205a67150921182f5ca
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-07-01 16:47:32 -0400

    Renaming data_sizes to dataSizes.

--- src/classes/BinaryReader.py
+++ src/classes/BinaryReader.py
@@ -4,7 +4,7 @@
 
 import struct
 
-import data.data_sizes
+import data.dataSizes
 
 #
 # Class
@@ -38,48 +38,48 @@ class BinaryReader:
 		return valueBytes
 
 	def readFloat32(self) -> float:
-		self.checkSize(data.data_sizes.FLOAT32)
+		self.checkSize(data.dataSizes.FLOAT32)
 
-		valueBytes = self.readBytes(data.data_sizes.FLOAT32)
+		valueBytes = self.readBytes(data.dataSizes.FLOAT32)
 
 		value = struct.unpack(self.byteOrderSymbol + "f", valueBytes)[0]
 
-		self.position += data.data_sizes.FLOAT32
+		self.position += data.dataSizes.FLOAT32
 
 		return value
 
 	# TODO: readFloat64 (idk how python handles 64-bit floats)
 
 	def readInt8(self) -> int:
-		self.checkSize(data.data_sizes.INT8)
+		self.checkSize(data.dataSizes.INT8)
 
-		valueBytes = self.readBytes(data.data_sizes.INT8)
+		valueBytes = self.readBytes(data.dataSizes.INT8)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = True)
 
-		self.position += data.data_sizes.INT8
+		self.position += data.dataSizes.INT8
 
 		return value
 
 	def readInt16(self) -> int:
-		self.checkSize(data.data_sizes.INT16)
+		self.checkSize(data.dataSizes.INT16)
 
-		valueBytes = self.readBytes(data.data_sizes.INT16)
+		valueBytes = self.readBytes(data.dataSizes.INT16)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = True)
 
-		self.position += data.data_sizes.INT16
+		self.position += data.dataSizes.INT16
 
 		return value
 
 	def readInt32(self) -> int:
-		self.checkSize(data.data_sizes.INT32)
+		self.checkSize(data.dataSizes.INT32)
 
-		valueBytes = self.readBytes(data.data_sizes.INT32)
+		valueBytes = self.readBytes(data.dataSizes.INT32)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = True)
 
-		self.position += data.data_sizes.INT32
+		self.position += data.dataSizes.INT32
 
 		return value
 
@@ -93,35 +93,35 @@ class BinaryReader:
 		return value
 
 	def readUint8(self) -> int:
-		self.checkSize(data.data_sizes.UINT8)
+		self.checkSize(data.dataSizes.UINT8)
 
-		valueBytes = self.readBytes(data.data_sizes.UINT8)
+		valueBytes = self.readBytes(data.dataSizes.UINT8)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = False)
 
-		self.position += data.data_sizes.UINT8
+		self.position += data.dataSizes.UINT8
 
 		return value
 
 	def readUint16(self) -> int:
-		self.checkSize(data.data_sizes.UINT16)
+		self.checkSize(data.dataSizes.UINT16)
 
-		valueBytes = self.readBytes(data.data_sizes.UINT16)
+		valueBytes = self.readBytes(data.dataSizes.UINT16)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = False)
 
-		self.position += data.data_sizes.UINT16
+		self.position += data.dataSizes.UINT16
 
 		return value
 
 	def readUint32(self) -> int:
-		self.checkSize(data.data_sizes.UINT32)
+		self.checkSize(data.dataSizes.UINT32)
 
-		valueBytes = self.readBytes(data.data_sizes.UINT32)
+		valueBytes = self.readBytes(data.dataSizes.UINT32)
 
 		value = int.from_bytes(valueBytes, byteorder = self.byteOrderString, signed = False)
 
-		self.position += data.data_sizes.UINT32
+		self.position += data.dataSizes.UINT32
 
 		return value
 
