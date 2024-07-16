37955bbfd00feb69d4bc4671c27b8cd1afa0b217
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-07-16 09:42:50 -0400

    Update defaultChunkRegistry.py
    Sorting some stuff.

--- src/instances/defaultChunkRegistry.py
+++ src/instances/defaultChunkRegistry.py
@@ -13,16 +13,16 @@ from classes.chunks.ImageDataChunk import ImageDataChunk
 from classes.chunks.IndexListChunk import IndexListChunk
 from classes.chunks.MeshChunk import MeshChunk
 from classes.chunks.OldPrimitiveGroupChunk import OldPrimitiveGroupChunk
+from classes.chunks.PathChunk import PathChunk
 from classes.chunks.PositionListChunk import PositionListChunk
 from classes.chunks.ShaderChunk import ShaderChunk
 from classes.chunks.ShaderColourParameterChunk import ShaderColourParameterChunk
 from classes.chunks.ShaderFloatParameterChunk import ShaderFloatParameterChunk
 from classes.chunks.ShaderIntegerParameterChunk import ShaderIntegerParameterChunk
 from classes.chunks.ShaderTextureParameterChunk import ShaderTextureParameterChunk
+from classes.chunks.StaticEntityChunk import StaticEntityChunk
 from classes.chunks.TextureChunk import TextureChunk
 from classes.chunks.UVListChunk import UVListChunk
-from classes.chunks.PathChunk import PathChunk
-from classes.chunks.StaticEntityChunk import StaticEntityChunk
 
 from classes.ChunkRegistry import ChunkRegistry
 
@@ -34,27 +34,25 @@ import data.chunkIdentifiers as chunkIdentifiers
 
 defaultChunkRegistry = ChunkRegistry()
 
-defaultChunkRegistry.register(chunkIdentifiers.HISTORY, HistoryChunk)
-
 defaultChunkRegistry.register(chunkIdentifiers.FENCE, FenceChunk)
 
 defaultChunkRegistry.register(chunkIdentifiers.FENCE_2, Fence2Chunk)
 
-defaultChunkRegistry.register(chunkIdentifiers.MESH, MeshChunk)
-
-defaultChunkRegistry.register(chunkIdentifiers.OLD_PRIMITIVE_GROUP, OldPrimitiveGroupChunk)
-
-defaultChunkRegistry.register(chunkIdentifiers.POSITION_LIST, PositionListChunk)
-
-defaultChunkRegistry.register(chunkIdentifiers.INDEX_LIST, IndexListChunk)
-
-defaultChunkRegistry.register(chunkIdentifiers.TEXTURE, TextureChunk)
+defaultChunkRegistry.register(chunkIdentifiers.HISTORY, HistoryChunk)
 
 defaultChunkRegistry.register(chunkIdentifiers.IMAGE, ImageChunk)
 
 defaultChunkRegistry.register(chunkIdentifiers.IMAGE_DATA, ImageDataChunk)
 
-defaultChunkRegistry.register(chunkIdentifiers.UV_LIST, UVListChunk)
+defaultChunkRegistry.register(chunkIdentifiers.INDEX_LIST, IndexListChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.MESH, MeshChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.OLD_PRIMITIVE_GROUP, OldPrimitiveGroupChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.PATH, PathChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.POSITION_LIST, PositionListChunk)
 
 defaultChunkRegistry.register(chunkIdentifiers.SHADER, ShaderChunk)
 
@@ -66,6 +64,8 @@ defaultChunkRegistry.register(chunkIdentifiers.SHADER_INTEGER_PARAMETER, ShaderI
 
 defaultChunkRegistry.register(chunkIdentifiers.SHADER_TEXTURE_PARAMETER, ShaderTextureParameterChunk)
 
-defaultChunkRegistry.register(chunkIdentifiers.PATH, PathChunk)
-
 defaultChunkRegistry.register(chunkIdentifiers.STATIC_ENTITY, StaticEntityChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.TEXTURE, TextureChunk)
+
+defaultChunkRegistry.register(chunkIdentifiers.UV_LIST, UVListChunk)
