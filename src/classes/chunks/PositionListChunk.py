3612debddb5f488d2cdd0dc0523e7f641f8ce01e
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-07-16 09:30:06 -0400

    Update PositionListChunk.py

--- src/classes/chunks/PositionListChunk.py
+++ src/classes/chunks/PositionListChunk.py
@@ -29,7 +29,7 @@ class PositionListChunk(Chunk):
 		for i in range(numberOfPositions):
 			positions.append(binaryReader.readPure3DVector3())
 
-		return [positions]
+		return [ positions ]
 
 	def __init__(self, identifier: int = chunkIdentifiers.POSITION_LIST, children : list[Chunk] = [], positions: list[mathutils.Vector] = []) -> None:
 		super().__init__(identifier,children)
