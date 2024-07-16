4ef44ba881951ca7ac43341980e437332dd1c4b1
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2024-07-16 09:26:45 -0400

    Update ImageChunk.py
    Formatting.

--- src/classes/chunks/ImageChunk.py
+++ src/classes/chunks/ImageChunk.py
@@ -55,7 +55,7 @@ class ImageChunk(Chunk):
 		hasAlpha = binaryReader.readUInt32()
 		format = binaryReader.readUInt32()
 
-		return [name, version, width, height, bitsPerPixel, palettized, hasAlpha, format]
+		return [ name, version, width, height, bitsPerPixel, palettized, hasAlpha, format ]
 
 	def __init__(self, identifier: int = chunkIdentifiers.IMAGE, children: list[Chunk] = [], name: str = "", version: int = 0, width: int = 0, height: int = 0, bitsPerPixel: int = 0, palettized: int = 0, hasAlpha: int = 0, format: int = 0) -> None:
 		super().__init__(chunkIdentifiers.IMAGE, children)
@@ -71,9 +71,7 @@ class ImageChunk(Chunk):
 
 	def writeData(self, binaryWriter : Pure3DBinaryWriter) -> None:
 		binaryWriter.writePure3DString(self.name)
-
 		binaryWriter.writeUInt32(self.version)
-
 		binaryWriter.writeUInt32(self.width)
 		binaryWriter.writeUInt32(self.height)
 		binaryWriter.writeUInt32(self.bitsPerPixel)
