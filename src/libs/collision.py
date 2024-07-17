292058871460f0cce6102ed08107336945ecd377
chocolateimage <chocolateimage@protonmail.com>
2024-07-17 23:18:39 +0200

    Make it work a little bit more

--- src/libs/collision.py
+++ src/libs/collision.py
@@ -27,15 +27,28 @@ def createFromVolume(collisionObject: CollisionObjectChunk, collisionVolume: Col
 		elif isinstance(child, CollisionOrientedBoundingBoxChunk):
 			centerChunk, rotationMatrixXChunk, rotationMatrixYChunk, rotationMatrixZChunk = child.getChildrenOfType(CollisionVectorChunk)
 			matrix = mathutils.Matrix((
-				(rotationMatrixXChunk.vector.x,rotationMatrixXChunk.vector.y,rotationMatrixXChunk.vector.z,0),
-				(rotationMatrixYChunk.vector.x,rotationMatrixYChunk.vector.y,rotationMatrixYChunk.vector.z,0),
-				(rotationMatrixZChunk.vector.x,rotationMatrixZChunk.vector.y,rotationMatrixZChunk.vector.z,0),
+				(rotationMatrixXChunk.vector.x,rotationMatrixYChunk.vector.x,rotationMatrixZChunk.vector.x,0),
+				(rotationMatrixXChunk.vector.y,rotationMatrixYChunk.vector.y,rotationMatrixZChunk.vector.y,0),
+				(rotationMatrixXChunk.vector.z,rotationMatrixYChunk.vector.z,rotationMatrixZChunk.vector.z,0),
 				(0,0,0,1),
 			))
+			euler: mathutils.Euler = matrix.to_euler()
+			#euler.rotate_axis("Z",180)
+			"""bpy.ops.mesh.primitive_cube_add(
+				location=(centerChunk.vector.x, centerChunk.vector.z, centerChunk.vector.y),
+				rotation=euler.,
+				size=2,
+				scale=(child.halfExtents.x, child.halfExtents.y, child.halfExtents.z),
+			)
+			obj = bpy.context.active_object
+			for collection in obj.users_collection:
+				collection.objects.unlink(obj)
+			obj.name = collisionObject.name+"Box"
+			objects.append(obj)"""
 			obj = bpy.data.objects.new(collisionObject.name+"Box",None)
 			obj.empty_display_size = 1
 			obj.empty_display_type = "CUBE"
-			obj.rotation_euler = matrix.to_euler()
+			obj.rotation_euler = mathutils.Euler((euler.x,euler.z,euler.y))
 			obj.location = centerChunk.vector.xzy
 			obj.scale = child.halfExtents.xzy
 			objects.append(obj)
